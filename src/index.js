f62441fcb76a16aaeb5d709fed0c1e1a3b3e34cb
James Kelly <jk_jameskelly@outlook.com>
2017-07-20 10:38:02 -0400

    basic setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {
+  ApolloProvider,
+  createNetworkInterface,
+  ApolloClient
+} from 'react-apollo';
+
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj5blhftlrfig0133mtmoidd3'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
+
+registerServiceWorker();
