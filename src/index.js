a0db8e57746dcf292f8962c6a1760ee32b12d56b
James Kelly <jk_jameskelly@outlook.com>
2017-07-20 15:13:56 -0400

    authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { GC_AUTH_TOKEN } from './constants'; 
 
 import {
   ApolloProvider,
@@ -15,14 +17,27 @@ const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj5blhftlrfig0133mtmoidd3'
 });
 
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN);
+    req.options.headers.authorization = token ? `Bearer ${token}` : null;
+    next();
+  }
+}])
+
 const client = new ApolloClient({
   networkInterface
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>, 
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 
